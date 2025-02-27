d347e5507d401e1a5b1fa38e634af4d46ece88bb
Leona202161 <bowanglin@gmail.com>
2025-02-26 20:03:41 -0800

    Updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,20 @@ public class Main {
         // Unit Test for Cash class
 
         // Test setup
-        Exchange exchange = new NYSE("someAccessKey");
-        Cash dollar = new Cash(exchange, 100);
+        Exchange exchange = new FakeExchange();  
+        Cash dollar = new Cash(exchange, 10000);  
         System.out.println("Dollar: " + dollar.toString());
 
-        // Test 'in' method of Cash
+        // Test 'in' method of Cash (Convert to Euros)
         Cash euro = dollar.in("Euro");
         System.out.println("Dollar to Euro: " + euro.toString());
+    }
+}
+
+// Fake Exchange class that provides a fixed exchange rate
+class FakeExchange implements Exchange {
+    @Override
+    public float rate(String origin, String target) {
+        return 1.2f;
     }
 }
